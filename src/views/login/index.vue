403b7eb38e19c38ecd3e30b49c797df5bf3bc5c4
侯帅 <houshuai0929@gmail.com>
2022-10-25 17:53:11 +0800

    完成了登陆页面

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -2,7 +2,31 @@
   <div class="login-page">
     <el-card class="el-card">
       <template #header>黑马面经运营后台</template>
-      <span>表单区域</span>
+      <!-- model：指定一个数据对象，对象中的属性都会和表单元素双向绑定 -->
+      <!-- rules：指定校验规则对象 -->
+      <el-form :model="loginForm" :rules="loginRules" ref="loginRef">
+        <!-- prop：指定该表单验证哪个值 -->
+        <el-form-item label="用户名" prop="username">
+          <!-- v-model：指定表单双向绑定的数据，来自于 model 指定的对象中的某个属性 -->
+          <el-input
+            placeholder="输入用户名"
+            v-model="loginForm.username"
+          ></el-input>
+        </el-form-item>
+
+        <el-form-item label="密码" prop="password">
+          <el-input
+            type="password"
+            placeholder="输入用户密码"
+            v-model="loginForm.password"
+          ></el-input>
+        </el-form-item>
+
+        <el-form-item class="tc">
+          <el-button type="primary">登 录</el-button>
+          <el-button>重 置</el-button>
+        </el-form-item>
+      </el-form>
     </el-card>
   </div>
 </template>
@@ -11,7 +35,13 @@
 export default {
   name: "LoginIndex",
   data() {
-    return {};
+    return {
+      loginForm: {
+        username: "",
+        password: "",
+      },
+      loginRules: {},
+    };
   },
   methods: {},
 };
